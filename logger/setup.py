13efe5c681ea1d2b1d61c97f3969979b9609639a
Sebastien Williams-Wynn <s.williamswynn.mail@gmail.com>
2019-05-08 23:16:04 +0100

    Allow logger to be parametrizable and decouple handlers

--- logger/setup.py
+++ logger/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="revlibs-logger",
-    version="0.2.0",
+    version="0.3.0",
     author="Demeter Sztanko",
     author_email="demeter.sztanko@revolut.com",
     packages=find_packages(),
@@ -11,8 +11,10 @@ setup(
     python_requires=">=3.6",
     install_requires=[
         "pyaml>=18.11.0",
-        "google-cloud-logging>=1.10.0",
-        "slacker-log-handler>=1.7.1",
     ],
+    extra_requires={
+        "slack": ["slacker-log-handler>=1.7.1"],
+        "stackdriver": ["google-cloud-logging>=1.10.0"],
+    },
     namespace_packages=["revlibs"],
 )
