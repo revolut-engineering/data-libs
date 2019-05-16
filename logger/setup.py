14640daac625b5135d61a64a39fc28e6780e63de
Vincenzo Musco <muscovin@gmail.com>
2019-05-16 10:11:12 +0100

    Version bump.

--- logger/setup.py
+++ logger/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="revlibs-logger",
-    version="0.3.2",
+    version="0.3.3",
     author="Demeter Sztanko",
     author_email="demeter.sztanko@revolut.com",
     packages=find_packages(),
