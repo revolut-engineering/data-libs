b139cdbbe133f7e958c218ce83c5eddb09c4c77f
Vincenzo Musco <muscovin@gmail.com>
2019-05-17 18:02:59 +0100

    Removing connection dependency to logger

--- connections/setup.py
+++ connections/setup.py
@@ -3,14 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="revlibs-connections",
-    version="0.1.0",
+    version="0.1.1",
     author="Demeter Sztanko",
     author_email="demeter.sztanko@revolut.com",
     packages=find_packages(),
     python_requires=">=3.6",
     install_requires=[
         "revlibs-dicts>=0.0.1",
-        "revlibs-logger>=0.0.2",
         "psycopg2-binary>=2.8",
         "pyexasol>=0.6",
     ],
